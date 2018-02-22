13f415255fcf652c6eb4b154d5e105020b048b8d
CSalt <csaltcoderesource@itu.edu.pk>
2018-02-23 04:17:57 +0500

    Initial commit

---
+++ Database/Raahemaa_Test1.sql
@@ -0,0 +1,62 @@
+﻿# Host: 127.0.0.1  (Version 5.6.24)
+# Date: 2017-07-31 16:43:15
+# Generator: MySQL-Front 6.0  (Build 2.20)
+
+
+#
+# Structure for table "feedback"
+#
+
+DROP TABLE IF EXISTS `feedback`;
+CREATE TABLE `feedback` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `uid` int(11) NOT NULL,
+  `Call_ID` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `recorded` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=79 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+#
+# Structure for table "files"
+#
+
+DROP TABLE IF EXISTS `files`;
+CREATE TABLE `files` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `file_name` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `prompt` varchar(256) COLLATE utf8_unicode_ci DEFAULT NULL,
+  `type` varchar(32) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+#
+# Structure for table "log"
+#
+
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE `log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fileid` int(11) NOT NULL,
+  `uid` int(11) NOT NULL,
+  `action` varchar(256) COLLATE utf8_unicode_ci NOT NULL,
+  `Call_ID` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12514 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+#
+# Structure for table "preference"
+#
+
+DROP TABLE IF EXISTS `preference`;
+CREATE TABLE `preference` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `fileid` int(11) NOT NULL,
+  `preference` varchar(12) COLLATE utf8_unicode_ci NOT NULL,
+  `uid` int(11) NOT NULL,
+  `timestamp` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `Call_ID` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=556 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
